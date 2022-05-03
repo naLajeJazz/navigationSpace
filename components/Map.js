9289d41c1d69fb9bd6d0d9b111ba8ff5e40f847a
Rodrigo Melo <nalajejazz@gmail.com>
2022-05-02 21:24:05 -0300

    cmt

--- components/Map.js
+++ components/Map.js
@@ -4,21 +4,12 @@ import Estilo from '../FolhaEstilo/FolhaEstilo'
 import { View,Text  } from 'react-native';
 
 export default () => {
-const simbol=['.','•','',]
-
-const [territorio1,setTerritorio1]=useState(simbol[Math.floor(Math.random()*simbol.length )])
 
   return (
 
 <Fragment >
 
-<View style={Estilo.containerMapa}>
-<CaixaMap>{territorio1}</CaixaMap>
-<CaixaMap>{territorio1}</CaixaMap>
-<CaixaMap>{territorio1}</CaixaMap>
-<CaixaMap>{territorio1}</CaixaMap>
-<CaixaMap>{territorio1}</CaixaMap>
-</View>
+
 
 </Fragment>
 
